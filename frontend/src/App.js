7b728ffcb0684ade709e3f19f024d4fe2fe4070c
rahul <rahulkr.mits@gmail.com>
2022-06-30 16:33:57 +0530

    images added, feature product implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import React from 'react';
+import data from './data';
 
 function App() {
   return (
@@ -6,7 +8,27 @@ function App() {
       <header>
         <a href="/">Amazona</a>
       </header>
-      <main>product list</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>{product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
